e2f99207a6071c1639da662b54d3686971318692
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-02 14:10:10 +0700

    Add Single Responsibility example

---
+++ SingleResponsibility/SingleResponsibility/Journal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class Journal
+    {
+        private readonly List<string> _entries = new List<string>();
+
+        private static int _count = 0;
+
+        public int AddEntry(string text)
+        {
+            _entries.Add($"{++_count}: {text}");
+            return _count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            _entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, _entries);
+        }
+
+        //public void Save(string filename)
+        //{
+        //    File.WriteAllText(filename, ToString());
+        //}
+
+        //public static Journal Load(string filename)
+        //{
+        //}
+
+        //public void Load(Uri uri)
+        //{
+        //}
+    }
+}
--- SingleResponsibility/SingleResponsibility/Program.cs
+++ SingleResponsibility/SingleResponsibility/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SingleResponsibility
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var journal = new Journal();
+            journal.AddEntry("The Hobbit");
+            journal.AddEntry("The Lord Of The Ring");
+            Console.WriteLine(journal);
+
+            var filename = @"D:\test\journal.txt";
+
+            var persistence = new Persistence();
+            persistence.SaveToFile(journal, filename, true);
+            Process.Start(filename);
         }
     }
 }
