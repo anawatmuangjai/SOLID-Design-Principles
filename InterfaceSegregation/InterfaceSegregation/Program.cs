c8cb40523111b8da49a0df5358a7e2ee97705b23
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-04 09:48:02 +0700

    Add Interface Segregation example

--- InterfaceSegregation/InterfaceSegregation/Program.cs
+++ InterfaceSegregation/InterfaceSegregation/Program.cs
@@ -8,8 +8,115 @@ namespace InterfaceSegregation
 {
     class Program
     {
+        public class Document
+        {
+
+        }
+
+        // Old
+        public interface IMachine
+        {
+            void Print();
+            void Scan();
+            void Fax();
+        }
+
+        // Old
+        public class MultiFunction : IMachine
+        {
+            public void Fax()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // Old
+        public class OldFashionedPrinter : IMachine
+        {
+            public void Fax()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Print()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // New
+        public interface IPrinter
+        {
+            void Print(Document d);
+        }
+
+        // New
+        public interface IScanner
+        {
+            void Scan(Document d);
+        }
+
+        // New
+        public class Photocopier : IPrinter, IScanner
+        {
+            public void Print(Document d)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Scan(Document d)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // New
+        public interface IMultiFunctionDevice : IPrinter, IScanner
+        {
+
+        }
+
+        // New
+        public class MultiFunctionMachine : IMultiFunctionDevice
+        {
+            private IPrinter _printer;
+            private IScanner _scanner;
+
+            public MultiFunctionMachine(IPrinter printer,IScanner scanner)
+            {
+                _printer = printer;
+                _scanner = scanner;
+            }
+
+            public void Print(Document d)
+            {
+                _printer.Print(d);
+            }
+
+            public void Scan(Document d)
+            {
+                _scanner.Scan(d);
+            }
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
