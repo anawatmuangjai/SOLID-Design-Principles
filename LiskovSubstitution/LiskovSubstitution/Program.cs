a66a62716de36bf89d07d067112411ba33b164fd
Anawat Muangjai <anawat.muangjai@gmail.com>
2017-10-04 09:13:49 +0700

    Add Liskov Substitution example

--- LiskovSubstitution/LiskovSubstitution/Program.cs
+++ LiskovSubstitution/LiskovSubstitution/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace LiskovSubstitution
 {
-    class Program
+    public class Program
     {
+        public static int Area(Rectangle r) => r.Width * r.Height;
+
         static void Main(string[] args)
         {
+            Rectangle rc = new Rectangle(5,5);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Rectangle square = new Square();
+            square.Width = 4;
+            Console.WriteLine($"{square} has area {Area(square)}");
         }
     }
 }
